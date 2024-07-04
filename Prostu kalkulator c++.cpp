9e8b6c97da3509a9c3164e124eafab48f792e417
Wilot <148060857+Wilot23@users.noreply.github.com>
2024-07-03 17:22:41 -0700

    Add files via upload

---
+++ Prostu kalkulator c++.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+	int jm;
+	float pierwszalicz;
+	float drugalicz;
+	cout << "wybierz pierwsza liczbe";
+	cin >> pierwszalicz;
+	cout << "wybierz druga liczbe";
+	cin >> drugalicz;
+	cout << "Wybierz czynnosc (* = 1 , / = 2 , - = 3 , + = 4)";
+	cin >> jm;
+		switch (jm) {
+		case 1:
+			cout << pierwszalicz * drugalicz << " oto twoj wynik";
+			break;
+
+		case 2:
+			cout << pierwszalicz / drugalicz << " oto twoj wynik";
+			break;
+		case 3:
+			cout << pierwszalicz - drugalicz << " oto twoj wynik";
+			break;
+		case 4:
+			cout << pierwszalicz + drugalicz << "oto twoj wynik";
+			break;
+		}
+
+	
+
+	
+	
+	return 0;
+
+	
+}
